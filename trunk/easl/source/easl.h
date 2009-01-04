97d9d3d46f439ff339be1618bbc2d508dd1e9b4a
mackron <mackron@b669be4e-d898-11dd-87b6-9d1a99559f42>
2009-01-04 07:38:35 +0000

    Initial commit

---
+++ trunk/easl/source/easl.h
@@ -0,0 +1,35 @@
+/**
+*   \file   easl.h
+*   \author Dave Reid
+*   \brief  Primary header file for EASL.
+*/
+#ifndef __EASL_H_
+#define __EASL_H_
+
+#include <tchar.h>
+#include "ext_funcs.h"
+//#include "slow_string.h"
+//#include "utf_ext.h"
+
+namespace easl
+{
+
+/*#ifdef SSL_USING_FAST_STRING
+typedef fast_string<TCHAR> string;
+#else
+typedef slow_string<TCHAR> string;
+#endif
+
+
+namespace utf
+{
+#ifdef SSL_USING_FAST_STRING
+    typedef fast_string<char8_t> string;
+#else
+    typedef slow_string<char8_t> string;
+#endif
+}*/
+
+}
+
+#endif // __EASL_H_
